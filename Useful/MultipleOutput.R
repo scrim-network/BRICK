133ab8eacecfc8917b78654a79beb7e174b77aef
Tony Wong <twong@psu.edu>
2017-01-10 11:58:40 -0500

    adding  routines

---
+++ Useful/MultipleOutput.R
@@ -0,0 +1,43 @@
+##=============================================================================
+## Function to allow the assignment of multiple objects at once from a function
+## with multiple outputs.
+##
+## Questions? Tony Wong (twong@psu.edu)
+##==============================================================================
+## Copyright 2016 Tony Wong, Alexander Bakker
+## This file is part of BRICK (Building blocks for Relevant Ice and Climate
+## Knowledge). BRICK is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## BRICK is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with BRICK.  If not, see <http://www.gnu.org/licenses/>.
+##==============================================================================
+library(methods)
+
+':=' <- function(lhs, rhs) {
+  frame <- parent.frame()
+  lhs <- as.list(substitute(lhs))
+  if (length(lhs) > 1)
+    lhs <- lhs[-1]
+  if (length(lhs) == 1) {
+    do.call(`=`, list(lhs[[1]], rhs), envir=frame)
+    return(invisible(NULL))
+  }
+  if (is.function(rhs) || is(rhs, 'formula'))
+    rhs <- list(rhs)
+  if (length(lhs) > length(rhs))
+    rhs <- c(rhs, rep(list(NULL), length(lhs) - length(rhs)))
+  for (i in 1:length(lhs))
+    do.call(`=`, list(lhs[[i]], rhs[[i]]), envir=frame)
+  return(invisible(NULL))
+}
+##=============================================================================
+## End
+##=============================================================================
