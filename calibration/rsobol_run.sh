8bd8bcb03f9f6b43ea795826232338b14cad9dfc
Anthony Wong <axw322@napa.local>
2017-03-10 12:48:34 -0500

    log filing working; use /dev/null for -e and -o when qsub invoked

--- calibration/rsobol_run.sh
+++ calibration/rsobol_run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-R --vanilla < /home/scrim/axw322/codes/BRICK/calibration/BRICK_Sobol_driver_script.R > test.log 2> test.log
+R --vanilla < /home/scrim/axw322/codes/BRICK/calibration/BRICK_Sobol_driver_script.R > /home/scrim/axw322/codes/BRICK/calibration/brick_sobol.log
+rm /home/scrim/axw322/codes/BRICK/calibration/rsobol_run.sh.*
--- calibration/rsobol_run2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-R CMD BATCH /home/scrim/axw322/codes/BRICK/calibration/BRICK_Sobol_driver_script.R
